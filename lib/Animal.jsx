800d0980c26c11c318d546271345252fc3deb9bb
Koki Takahashi <hakatasiloving@gmail.com>
2017-03-18 19:32:16 +0900

    Animals go clickable

--- lib/Animal.jsx
+++ lib/Animal.jsx
@@ -1,5 +1,7 @@
 const React = require('react');
+const CSSModules = require('react-css-modules');
 const {translate} = require('./util.js');
+const styles = require('./Animal.css');
 
 const startPoint = [5, 55];
 const startAngle = Math.PI / 180 * 90;
@@ -11,14 +13,21 @@ class Animal extends React.Component {
 		super(props, state);
 	}
 
+	onClick(event) {
+		event.preventDefault();
+		this.props.onClick(this.props.name);
+	}
+
 	render() {
 		return (
 			<g transform={translate(this.props)}>
-				<circle cx="0" cy="0" r="10" fill="pink" stroke={this.props.active ? 'red' : 'none'} />
-				<text x="0" y="0" fontSize="4px" textAnchor="middle" fontWeight="bold">{this.props.name}</text>
+				<g styleName="animal" data-active={this.props.active} onClick={this.onClick.bind(this)}>
+					<circle cx="0" cy="0" r="10" fill="pink" stroke={this.props.active ? 'red' : 'none'} />
+					<text x="0" y="0" fontSize="4px" textAnchor="middle" fontWeight="bold">{this.props.name}</text>
+				</g>
 			</g>
 		)
 	}
 }
 
-module.exports = Animal;
+module.exports = CSSModules(Animal, styles);
--- lib/App.jsx
+++ lib/App.jsx
@@ -38,13 +38,18 @@ class App extends React.Component {
 		};
 	}
 
+	onClickAnimal(name) {
+		this.setState({activeAnimal: name});
+	}
+
 	render() {
 		return (
 			<svg styleName="root" viewBox="0 0 200 100" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
 				<image x="0" y="0" width="100%" height="100%" xlinkHref="img/park_editable.svg" />
+				<rect  />
 				<Snake x={0} y={0} />
 				{this.state.animals.map((animal) => (
-					<Animal key={animal.name} x={animal.x} y={animal.y} name={animal.name} active={this.state.activeAnimal === animal.name} />
+					<Animal key={animal.name} x={animal.x} y={animal.y} name={animal.name} active={this.state.activeAnimal === animal.name} onClick={this.onClickAnimal.bind(this)} />
 				))}
 			</svg>
 		);
