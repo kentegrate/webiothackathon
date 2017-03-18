a45946197bd8bbacf6c92b9bb5d0bf983babc0ea
Koki Takahashi <hakatasiloving@gmail.com>
2017-03-18 13:23:24 +0900

    Implement log function

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const textarea = document.getElementById('textarea');
 let state = false;
 
+const log = (text) => {
+	console.log(text);
+	textarea.value += `${text}\n`;
+};
+
 window.addEventListener('load', () => {
 	navigator.requestGPIOAccess().then((gpio) => {
 		console.log('gpio:', gpio);
-		const ports = gpio.ports;
-
-		textarea.value = JSON.stringify(ports, '  ', 2);
-
 		const port = gpio.ports.get(244);
 		return port.export('out').then(() => port);
 	}).then((port) => {
@@ -17,9 +18,8 @@ window.addEventListener('load', () => {
 		setInterval(() => {
 			state = !state;
 			port.write(state ? 1 : 0);
+			log(state ? 'on' : 'off');
 			document.body.style.backgroundColor = state ? 'red' : 'white';
 		}, 1000);
 	});
-
-	setTimeout(() => { location.reload(true); }, 20000);
 });
