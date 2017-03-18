a9f2a2003ee1f983d3af727d21225d601ae9e9d9
TomonoriSuzuki <tomonori.suzuki@zeals.co.jp>
2017-03-18 13:44:08 +0900

    非同期処理テンプレート

--- index.js
+++ index.js
@@ -11,17 +11,25 @@ const log = (text) => {
 window.addEventListener('load', () => {
 	navigator.requestGPIOAccess().then((gpio) => {
 		console.log('gpio:', gpio);
+		
 		const port = gpio.ports.get(244);
-		return port.export('out').then(() => port);
-	}).then((port) => {
-		console.log('port:', port);
-		port.write(1);
-		document.body.style.backgroundColor = 'red';
-		setInterval(() => {
-			state = !state;
-			port.write(state ? 1 : 0);
-			log(state ? 'on' : 'off');
-			document.body.style.backgroundColor = state ? 'red' : 'white';
-		}, 1000);
+		port.export('out').then(() => {
+			console.log('port:', port);
+			port.write(1);
+			document.body.style.backgroundColor = 'red';
+			setInterval(() => {
+				state = !state;
+				port.write(state ? 1 : 0);
+				log(state ? 'on' : 'off');
+				document.body.style.backgroundColor = state ? 'red' : 'white';
+			}, 1000);
+		});
+		
+		const port2 = gpio.ports.get(244);
+		port2.export('out').then(() => {
+			console.log('port:', port2);
+
+			
+		});
 	});
 });
