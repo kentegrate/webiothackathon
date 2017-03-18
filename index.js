101c4c1c4579e34c139d32693b343d3e0cf35383
Koki Takahashi <hakatasiloving@gmail.com>
2017-03-18 11:51:21 +0900

    Include webgpio.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ console.log('Welcome to CHIRIMEN');
 textarea.value = 'CHIRIMEN';
 window.addEventListener('load', () => {
 	try {
-			textarea.value = 'CHIRIMEN2';
-			navigator.requestGPIOAccess().then((gpio) => {
-				const ports = gpio.ports;
+		textarea.value = 'CHIRIMEN2';
+		navigator.requestGPIOAccess().then((gpio) => {
+			const ports = gpio.ports;
 
-				textarea.value = JSON.stringify(ports, '  ', 2);
-			});
+			textarea.value = JSON.stringify(ports, '  ', 2);
+		});
 	} catch (error) {
 		console.error(error);
 		textarea.value = `ERROR: ${error}`;
