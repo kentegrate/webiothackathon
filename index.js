97b111af890d9d9a412afbbdb1035f7213d4a472
Koki Takahashi <hakatasiloving@gmail.com>
2017-03-18 12:01:22 +0900

    L chika

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const textarea = document.getElementById('textarea');
-alert('Welcome to CHIRIMEN!');
-console.log('Welcome to CHIRIMEN');
-textarea.value = 'CHIRIMEN';
-window.addEventListener('load', () => {
-	try {
-		textarea.value = 'CHIRIMEN2';
-		navigator.requestGPIOAccess().then((gpio) => {
-			const ports = gpio.ports;
+let state = false;
 
-			textarea.value = JSON.stringify(ports, '  ', 2);
-		});
-	} catch (error) {
-		console.error(error);
-		textarea.value = `ERROR: ${error}`;
-	}
+window.addEventListener('load', () => {
+	navigator.requestGPIOAccess().then((gpio) => {
+		console.log(gpio);
+		const ports = gpio.ports;
+
+		textarea.value = JSON.stringify(ports, '  ', 2);
+
+		const port = gpio.ports.get(198);
+		return port.export('out');
+	}).then(() => {
+		console.log('port exported');
+		setInterval(() => {
+			console.log(state ? 'on' : 'off');
+			port.write(state ? 1 : 0);
+
+			if (state) {
+				document.body.style.backgroundColor = 'red';
+			} else {
+				document.body.style.backgroundColor = 'white'
+			}
+
+			state = !state;
+		}, 1000);
+	});
 });
