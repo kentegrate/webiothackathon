ba6aea984e80b860b060cf33040b73f731282215
Koki Takahashi <hakatasiloving@gmail.com>
2017-03-18 17:34:32 +0900

    Add Animal.jsx

--- lib/App.jsx
+++ lib/App.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 const Snake = require('./Snake.jsx');
+const Animal = require('./Animal.jsx');
 
 class App extends React.Component {
 	constructor(props, state) {
@@ -8,8 +9,13 @@ class App extends React.Component {
 
 	render() {
 		return (
-			<svg viewBox="0 0 100 100" width="300px" height="300px" version="1.1" xmlns="http://www.w3.org/2000/svg">
+			<svg viewBox="0 0 100 100" width="300px" height="300px" version="1.1" xmlns="http://www.w3.org/2000/svg" style={{border: '1px solid black', background: 'white'}}>
 				<Snake x={0} y={0} />
+				<Animal x={60} y={60} name="オカピ" />
+				<Animal x={60} y={60} name="オカピ" />
+				<Animal x={70} y={50} name="キリン" />
+				<Animal x={90} y={80} name="シマウマ" />
+				<Animal x={10} y={30} name="ヘビ" />
 			</svg>
 		);
 	}
