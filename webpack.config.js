1077fe817fbd4885de772626739d1cc4a9351729
Koki Takahashi <hakatasiloving@gmail.com>
2017-03-18 14:47:01 +0900

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+	entry: './lib/index.js',
+	output: {
+		path: __dirname,
+		filename: 'index.js',
+	},
+	module: {
+	},
+};
