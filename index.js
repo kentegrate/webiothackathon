d02ccca13e7ae8e2ed0920c69fa5eaef10e04385
TomonoriSuzuki <tomonori.suzuki@zeals.co.jp>
2017-03-18 13:57:09 +0900

    まげセンサ

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let state = false;
 const log = (text) => {
 	console.log(text);
 	textarea.value += `${text}\n`;
-	textarea.value = "エイッ!!\n　　　　∧,,∧\n☆二　⊂(・ω・｀)\n　　　　-ヽ　　と)\n　　　　｀ｕ-ｕ'";
+	//textarea.value += "エイッ!!\n　　　　∧,,∧\n☆二　⊂(・ω・｀)\n　　　　-ヽ　　と)\n　　　　｀ｕ-ｕ'";
 	//textarea.scrollTop = textarea.scrollHeight;
 };
 
@@ -21,15 +21,14 @@ window.addEventListener('load', () => {
 				state = !state;
 				port.write(state ? 1 : 0);
 				log(state ? 'on' : 'off');
-				document.body.style.backgroundColor = state ? 'red' : 'white';
+				//document.body.style.backgroundColor = state ? 'red' : 'white';
 			}, 1000);
 		});
 		
-		const port2 = gpio.ports.get(244);
-		port2.export('out').then(() => {
+		const port2 = gpio.ports.get(193);
+		port2.export('in').then(() => {
 			console.log('port:', port2);
-
-			
+			textarea.value += port2.read();
 		});
 	});
 });
