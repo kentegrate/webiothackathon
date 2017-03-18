e39e3af0bcfcf46293db2e7f8b09adc3d12ea59e
TomonoriSuzuki <tomonori.suzuki@zeals.co.jp>
2017-03-18 14:29:58 +0900

    button_check

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ window.addEventListener('load', () => {
 	navigator.requestGPIOAccess().then((gpio) => {
 		console.log('gpio:', gpio);
 		
-		const port = gpio.ports.get(244);
+		/*const port = gpio.ports.get(244);
 		port.export('out').then(() => {
 			console.log('port:', port);
 			port.write(1);
@@ -21,9 +21,9 @@ window.addEventListener('load', () => {
 				state = !state;
 				port.write(state ? 1 : 0);
 				log(state ? 'on' : 'off');
-				//document.body.style.backgroundColor = state ? 'red' : 'white';
+				document.body.style.backgroundColor = state ? 'red' : 'white';
 			}, 1000);
-		});
+		});*/
 		
 		const port2 = gpio.ports.get(193);
 		port2.export('in').then(() => {
@@ -31,6 +31,14 @@ window.addEventListener('load', () => {
 			port2.read().then((value) => {
 				log(value);
 			});
+		});
+
+		const buttonPort = gpio.ports.get(199);
+		buttonPort.export('in').then(() => {
+			buttonPort.onchange = function(state) {
+				document.body.style.backgroundColor = state ? 'red' : 'white';
+				state != state;
+			}
 		});
 	});
 });
