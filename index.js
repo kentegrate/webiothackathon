595ea26e2b12669b19a36385f86efc0a9831b7c8
Koki Takahashi <hakatasiloving@gmail.com>
2017-03-18 12:55:17 +0900

    Add more console.logs

--- index.js
+++ index.js
@@ -13,10 +13,15 @@ window.addEventListener('load', () => {
 	}).then((port) => {
 		console.log('port:', port);
 		port.write(1);
+		console.log('wrote port');
 		document.body.style.backgroundColor = 'red';
+		console.log('red');
 		setInterval(() => {
+			console.log(state);
 			state = !state;
+			console.log(state);
 			port.write(state ? 1 : 0);
+			console.log('port wrote');
 			document.body.style.backgroundColor = state ? 'red' : 'white';
 		}, 1000);
 	});
