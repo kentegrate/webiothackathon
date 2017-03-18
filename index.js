735f5b0ee969221803cf5ceb755ef26e56f94e40
Koki Takahashi <hakatasiloving@gmail.com>
2017-03-18 12:49:20 +0900

    Export port variabl

--- index.js
+++ index.js
@@ -3,20 +3,19 @@ let state = false;
 
 window.addEventListener('load', () => {
 	navigator.requestGPIOAccess().then((gpio) => {
-		console.log(gpio);
+		console.log('gpio:', gpio);
 		const ports = gpio.ports;
 
 		textarea.value = JSON.stringify(ports, '  ', 2);
 
 		const port = gpio.ports.get(198);
-		return port.export('out');
-	}).then(() => {
-		console.log('port exported');
+		return port.export('out').then(() => port);
+	}).then((port) => {
+		console.log('port:', port);
 		port.write(1);
 		document.body.style.backgroundColor = 'red';
 		setInterval(() => {
 			state = !state;
-			console.log(state ? 'on' : 'off');
 			port.write(state ? 1 : 0);
 			document.body.style.backgroundColor = state ? 'red' : 'white';
 		}, 1000);
