cf7c4126067c05951caebdd1ae7dabe44d18af06
Koki Takahashi <hakatasiloving@gmail.com>
2017-03-19 10:59:22 +0900

    Implement connection to servo

--- lib/App.jsx
+++ lib/App.jsx
@@ -5,12 +5,14 @@ const Snake = require('./Snake.jsx');
 const Animal = require('./Animal.jsx');
 const styles = require('./App.css');
 const {translate, distance, angle} = require('./util.js');
+const pca9685 = require('./PCA9685.js')
 
 const currentPlace = {x: 137, y: 37};
 
 class App extends React.Component {
 	constructor(props, state) {
 		super(props, state);
+
 		this.state = {
 			animals: [{
 				x: 100,
@@ -31,19 +33,29 @@ class App extends React.Component {
 			}],
 			activeAnimal: null,
 		};
+
+		this.pca9685 = navigator.requestI2CAccess().then((i2cAccess) => {
+			const i2cPort = i2cAccess.ports.get(0);
+			return pca9685(i2cPort);
+		}).then((servo) => {
+			this.servo = servo;
+		});
 	}
 
 	onClickAnimal(name) {
 		this.setState({activeAnimal: name});
 		const activeAnimal = this.state.animals.find((animal) => animal.name === name);
-		console.log(distance(currentPlace, activeAnimal), angle(currentPlace, activeAnimal));
+
+		if (this.servo) {
+			this.servo.setServo(0, angle(currentPlace, activeAnimal) / Math.PI * 180)
+		}
 	}
 
 	render() {
 		return (
 			<svg styleName="root" viewBox="0 0 200 100" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
 				<image x="0" y="0" width="100%" height="100%" xlinkHref="img/park_editable.svg" />
-				<text x="100" y="15" fontSize="10" textAnchor="middle">東山動物園 西園 案内図</text>
+				<text x="100" y="15" fontSize="10" textAnchor="middle">上野動物園 西園 案内図</text>
 				<g transform={translate({x: 137, y: 37})}>
 					<circle cx="0" cy="0" r="2" fill="red" />
 					<text x="0" y="7" fontSize="5" textAnchor="middle">現在地</text>
