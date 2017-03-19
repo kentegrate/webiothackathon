46847f677fe0f5d77b0de00e32f8f7bbed579b5b
Koki Takahashi <hakatasiloving@gmail.com>
2017-03-19 11:39:29 +0900

    Modify angle calculation

--- lib/App.jsx
+++ lib/App.jsx
@@ -47,7 +47,7 @@ class App extends React.Component {
 		const activeAnimal = this.state.animals.find((animal) => animal.name === name);
 
 		if (this.servo) {
-			const servoAngle = (angle(currentPlace, activeAnimal) / Math.PI * 180 + 180 + 360 - 45) % 180;
+			const servoAngle = 180 - (angle(currentPlace, activeAnimal) / Math.PI * 180 + 180 + 360 - 45) % 180;
 			this.servo.setServo(0, servoAngle)
 		}
 	}
@@ -57,14 +57,14 @@ class App extends React.Component {
 			<svg styleName="root" viewBox="0 0 200 100" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
 				<image x="0" y="0" width="100%" height="100%" xlinkHref="img/park_editable.svg" />
 				<text x="100" y="15" fontSize="10" textAnchor="middle">上野動物園 西園 案内図</text>
-				<g transform={translate({x: 137, y: 37})}>
-					<circle cx="0" cy="0" r="2" fill="red" />
-					<text x="0" y="7" fontSize="5" textAnchor="middle">現在地</text>
-				</g>
 				<Snake x={33} y={51} />
 				{this.state.animals.map((animal) => (
 					<Animal key={animal.name} x={animal.x} y={animal.y} name={animal.name} active={this.state.activeAnimal === animal.name} onClick={this.onClickAnimal.bind(this)} />
 				))}
+				<g transform={translate({x: 137, y: 37})}>
+					<circle cx="0" cy="0" r="2" fill="red" />
+					<text x="0" y="7" fontSize="5" textAnchor="middle">現在地</text>
+				</g>
 			</svg>
 		);
 	}
